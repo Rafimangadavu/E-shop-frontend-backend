d8d0ecbc836c793fdb8a61546d2a42661d99bcd5
RafiMangadavu <rafimangadavu2025@gmail.com>
2022-10-19 20:46:23 +0530

    second commit

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dokrc7vnb/image/upload/v1665676081/Online%20Shop/iphone12pro_xa3dkc.jpg"
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/dokrc7vnb/image/upload/v1665676081/Online%20Shop/iphone12_nqx29t.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/dokrc7vnb/image/upload/v1665676081/Online%20Shop/galaxyS_ejc4pb.png"
+    },
+];
+
+module.exports = products;
